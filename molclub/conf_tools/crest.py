03d87855d08c2b5bfeda11165839081aa18f952a
Sam Mun <therealsam@berkeley.edu>
2022-08-05 09:05:49 -0700

    add crest capabilities

--- molclub/conf_tools/crest.py
+++ molclub/conf_tools/crest.py
@@ -1,5 +1,8 @@
 import subprocess
 from dataclasses import dataclass
+from tempfile import TemporaryDirectory
+from pathlib import Path
+from os.path import exists, isdir
 
 # import tempfile
 from typing import List, Optional, TextIO, Union
@@ -7,25 +10,63 @@ from typing import List, Optional, TextIO, Union
 from rdkit import Chem  # type: ignore
 
 from molclub.compute import compute_utils
+from molclub.conf_tools.conf_utils import (
+    mol_has_one_conf, conf_from_xyz, order_confs, prune
+)  # format
 
 
 def reopt_ensemble():
     pass
+    
+
+@dataclass(init=True, repr=True, slots=True)
+class Result(compute_utils.Result):
+    energies_kcal: List[float]
+    energies_hartree: List[float]
+    confs: List[Chem.Conformer]
+
+    def extract_results(
+        self,
+        cwd: str,
+    ) -> None:
+        # with open(f'{cwd}/crest.out') as crest_out:
+        xyz_blocks = []
+        with open(f'{cwd}/crest_conformers.xyz') as confs:
+            lines = confs.readlines()
+            num_atoms = int(lines[0].strip('\n'))
+            lines_per_entry = num_atoms + 2
+            xyz_block = ''
+            for i in range(0, len(lines)):
+                xyz_block += lines[i]
+                if (i + 1) % lines_per_entry == 0:
+                    xyz_blocks.append(xyz_block)
+                    xyz_block = ''
+
+        self.energies_hartree = []        
+        self.confs = []
+        for xyz_block in xyz_blocks:
+            xyz_block = xyz_block.split('\n')
+            self.energies_hartree.append(float(xyz_block[1]))
+            xyz = xyz_block[2:]
+            xyz.remove('')
+            self.confs.append(conf_from_xyz(xyz))
+        self.energies_kcal = [e * 627.5 for e in self.energies_hartree]
 
 
 @dataclass(init=True, repr=True, slots=True)
 class Parameters(compute_utils.Parameters):
+    # use_cregen: bool = False
     method: str = "gfnff"
     search_intensity: str = "full"
-    rmsd_thresh: float = 0.125
-    energy_thresh: float = 0.05
-    boltzmann_thresh: float = 0.05
-    energy_window: float = 10
+    # rmsd_thresh: float = 0.125
+    # energy_thresh: float = 0.05
+    # boltzmann_thresh: float = 0.05
+    # energy_window: float = 10
     solvation: str = "alpb"
     solvent: str = "water"
-    num_mtd_cycle: int = 5
-    opt_tightness: str = "vtight"
-    prop: Optional[str] = None
+    # num_mtd_cycle: int = 5
+    # opt_tightness: str = "vtight"
+    # prop: Optional[str] = None
     protonate: bool = False
     deprotonate: bool = False
     tautomerize: bool = False
@@ -46,19 +87,25 @@ class Parameters(compute_utils.Parameters):
             )  # format
         if self.solvation not in ["alpb", "gbsa"]:
             raise ValueError(f"{self.solvation} not a valid solvation method")
-        if self.opt_tightness not in [
-            "crude",
-            "sloppy",
-            "loose",
-            "lax",
-            "normal",
-            "tight",
-            "vtight",
-            "extreme",
-        ]:
-            raise ValueError(f"{self.opt_tightness} not a valid tightness")
-        if self.prop not in [None, "hess", "reopt", "autoIR"]:
-            raise ValueError(f"{self.prop} not a valid property calculation")
+        # if self.opt_tightness not in [
+        #     "crude",
+        #     "sloppy",
+        #     "loose",
+        #     "lax",
+        #     "normal",
+        #     "tight",
+        #     "vtight",
+        #     "extreme",
+        # ]:
+        #     raise ValueError(f"{self.opt_tightness} not a valid tightness")
+        # if self.prop not in [None, "hess", "reopt", "autoIR"]:
+        #     raise ValueError(f"{self.prop} not a valid property calculation")
+        if self.protonate and self.deprotonate:
+            raise ValueError('both protonate and deprotonate cannot be True')
+
+    # def get_use_cregen(self) -> List[str]:
+    #     if self.use_cregen:
+    #         return ['--cregen']
 
     def get_method(self) -> List[str]:
         method_dict = {
@@ -69,23 +116,113 @@ class Parameters(compute_utils.Parameters):
         }
         return method_dict[self.method]
 
-    # def get_search_settings(self) -> List[str]:
-    # search_intensity_dict = {
-    #     "full": []
-    #     "fast":
-    #     "faster":
-    #     "fastest":
-    # }
+    def get_search_settings(self) -> List[str]:
+        search_intensity_dict = {
+            "fast": ['--quick'],
+            "faster": ['--squick'],
+            "fastest": ['--mquick'],
+        }
+
+        return search_intensity_dict[self.search_intensity]
 
     def get_args(self) -> List[str]:
         args = []
+        # if self.use_cregen:
+        #     args += ['--cregen']
         args += self.get_method()
+        if self.search_intensity != 'full':
+            args += self.get_search_settings()
+        args += [f"--{self.solvation}", self.solvent]
+        # args += ['--mrest', str(self.num_mtd_cycle)]
+        if self.protonate:
+            args += ['--protonate']
+        if self.deprotonate:
+            args += ['--deprotonate']
+        if self.tautomerize:
+            args += ['--tautomerize']
+        args += ['--iter', str(self.taut_iter)]
+        args += ['--T', str(self.num_threads)]
 
         return args
 
 
 class crestError(Exception):
     pass
+
+
+def generate_conformers(
+    mol: Chem.Mol,
+    num_unpaired_electrons: int = 0,
+    prune_rms_thresh: float = 0.125,
+    crest_params: Parameters = Parameters(),
+) -> List[Chem.Mol]:
+    mol_has_one_conf(mol)
+    result = job(
+        mol=mol,
+        params=crest_params,
+        charge=Chem.GetFormalCharge(mol),
+        num_unpaired_electrons=num_unpaired_electrons,
+    )
+    mols = []
+    for conf in result.confs:
+        temp_mol = Chem.Mol(mol, quickCopy=True)
+        temp_mol.AddConformer(conf, assignId=True)
+        mols.append(temp_mol)
+
+    mols, energies = order_confs(mols, energies=result.energies_kcal)
+    mols, energies = prune(mols, energies, prune_rms_thresh)
+
+    return mols
+
+
+def job(
+    mol: Chem.Mol,
+    params: Parameters,
+    charge: int = 0,
+    num_unpaired_electrons: int = 0,
+    use_temp_dir: bool = True,
+    working_dir: Optional[str] = None,
+) -> Result:
+    mol_has_one_conf(mol)
+    if use_temp_dir and working_dir is not None:
+        raise ValueError("cannot specify working_dir if use_temp_dir = True")
+    if working_dir is not None:
+        if working_dir.startswith("~"):
+            working_dir = f"{str(Path.home())}/{working_dir[2:]}"
+    
+    crest_args = [
+        'crest',
+        'input.xyz',
+        '--chrg',
+        str(charge),
+        '--uhf',
+        str(num_unpaired_electrons),
+    ]
+    crest_args += params.get_args()
+
+    if use_temp_dir:
+        with TemporaryDirectory() as tmp:
+            Chem.MolToXYZFile(mol, f"{tmp}/input.xyz")
+            with open(f"{tmp}/crest.out", 'w') as crest_out:
+                run(crest_args=crest_args, cwd=tmp, crest_out=crest_out)
+            crest_result = Result(False, False, False)
+            crest_result.extract_results(cwd=tmp)
+    else:
+        assert isinstance(working_dir, str)
+        if not isdir(working_dir):
+            raise NotADirectoryError(f"{working_dir} is not a directory")
+        else:
+            Chem.MolToXYZFile(mol, f"{working_dir}/input.xyz")
+            with open(f"{working_dir}/crest.out", 'w') as crest_out:
+                run(
+                    crest_args=crest_args,
+                    cwd=working_dir,
+                    crest_out=crest_out,
+                )
+            crest_result = Result(False, False, False)
+            crest_result.extract_results(cwd=working_dir)
+    
+    return crest_result
 
 
 def run(
@@ -103,15 +240,3 @@ def run(
         raise crestError(proc.stderr.decode())
 
     return proc
-
-
-def job(
-    mol: Chem.Mol,
-    params: Parameters,
-    job_type: str = "mtd-gc",
-    charge: int = 0,
-    num_unpaired_electrons: int = 0,
-    use_temp_dir: bool = True,
-    working_dir: Optional[str] = None,
-):
-    pass
